d08c50de4c8c192fb1394ad28c5de718ed5a1744
rusanov-ao <rusanov-ao@inbox.ru>
2022-12-31 23:01:06 +0300

    up

--- src/main/scala/rdd/SparkRDD.scala
+++ src/main/scala/rdd/SparkRDD.scala
@@ -24,10 +24,26 @@ object SparkRDD {
 //      .saveAsTextFile("/home/alexander/for_spark/a1.txt")
 
 
-    val a = sc.textFile("/home/alexander/for_spark/a.txt")
-          .flatMap(line => line.split(" "))
-          .map(word => word + "=)")
-          .foreach(line => println(line))
+//    val a = sc.textFile("/home/alexander/for_spark/a.txt")
+//          .flatMap(line => line.split(" "))
+//          .map(word => word + "=)")
+//          .foreach(line => println(line))
+
+    val rdd = sc.textFile("/home/alexander/spark/README.md")
+
+    println(rdd.first())
+
+    println(s"Количество строк ${rdd.count()}")
+
+    val rdd2 = rdd
+      .filter(line => line.contains("spark"))
+      .foreach(line => println(line))
+
+    val rdd3 = rdd
+      .filter(word => word.contains("spark"))
+
+
+    println(s"\nКоличество повторяющихся слов spark равняется ${rdd3.count()}")
 
     //println(a.toString())
 
